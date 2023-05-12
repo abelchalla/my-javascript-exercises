15cbb1c2f7ad59ab41dfa3ac53206200bf17a82c
Abel Challa <abelchalla22@gmail.com>
2023-05-12 14:51:08 -0700

    Make a func that repeats a str, x amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, amount) {
+    if (amount < 0) return "ERROR";
+    let new_string = '';
+    for (let i = 0; i < amount; i++) {
+        new_string += string;
+    }
+    return new_string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
