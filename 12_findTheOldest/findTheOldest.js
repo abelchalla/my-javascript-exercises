6b3eacec1b4b4f935556dafec477b88c2ebd68d4
Abel Challa <abelchalla22@gmail.com>
2023-05-14 15:26:25 -0700

    Find the oldest person given an array of people objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let name;
+    let date = new Date().getFullYear();
+    for(let i = 0; i < people.length; i++){
+        if (people[i].yearOfDeath === undefined) {
+            if(date - people[i].yearOfBirth > oldest) {
+                oldest = date - people[i].yearOfBirth
+                name = people[i];
 
+            }
+        }
+        else if(people[i].yearOfDeath - people[i].yearOfBirth > oldest) {
+            oldest = people[i].yearOfDeath - people[i].yearOfBirth;
+            name = people[i];
+        }
+    }
+    return name;
 };
 
 // Do not edit below this line
