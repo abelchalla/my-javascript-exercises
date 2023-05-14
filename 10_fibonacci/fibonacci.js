6f1e5d43373b01acdb0933ed9e601aabb72384a4
Abel Challa <abelchalla22@gmail.com>
2023-05-14 13:50:46 -0700

    Create a function that returns the index of fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num <= 0) return 'OOPS';
+    if(num == 1 || num == 2) return 1;
+    let a = 1;
+    let b = 1;
+    let total = 2;
+    for (let i = 2; i < num - 1; i++) {
+        a = b;
+        b = total;
+        total = a + b; 
+    }
+    return total;
 };
 
 // Do not edit below this line
