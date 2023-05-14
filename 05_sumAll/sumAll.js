2f82d53ffba403bc0f0166d0e65511c774038893
Abel Challa <abelchalla22@gmail.com>
2023-05-13 21:04:25 -0700

    Add the numbers between the two arguments inclusively

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    if(begin < 0 || end < 0) return 'ERROR';
+    if (typeof(begin) != typeof(1) || typeof(end) != typeof(1)){
+        return 'ERROR';
+    }
+    let sum = 0;
+    if(end > begin){
+        for(let i = begin; i <= end; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        for(let i = end; i <= begin; i++){
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
