bae7184a387efd5d2ecf9a7da8288babbbc9d6d9
Abel Challa <abelchalla22@gmail.com>
2023-05-14 04:28:19 -0700

    Create a function that checks if a string is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (strings) {
+    strings = strings.toLowerCase();
+    let re = /[a-z]/g;
+    let newString = strings.match(re).join("");
+    for(let i = 0, j = newString.length - 1; i <= j; i++, j--) {
+        if(newString[i] != newString[j]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
