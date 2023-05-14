ab53ef88c17aec903e86e975420b0037fd9a524a
Abel Challa <abelchalla22@gmail.com>
2023-05-14 01:19:44 -0700

    Create multiple functions that act like a calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for(let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multi = 1;
+  for(let i = 0; i < array.length; i++) {
+    multi *= array[i];
+  }
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  let i = 0;
+  let answer = 1;
+	while(i < pow){
+    answer *= num;
+    i++;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  for (let i = 2; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
