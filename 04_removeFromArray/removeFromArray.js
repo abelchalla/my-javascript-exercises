dce3bd70499cbacb684c64f4ed6d701ae5309660
Abel Challa <abelchalla22@gmail.com>
2023-05-13 20:18:01 -0700

    Remove elements that match the arg from the given arr

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+       for (let j = 0; j < arr.length; j++) {
+        if(arguments[i] == arr[j] && typeof(arguments[i]) === typeof(arr[j])) {
+                arr.splice(j, 1);
+            }
+       }
+    }
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
